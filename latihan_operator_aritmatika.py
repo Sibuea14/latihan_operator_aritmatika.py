b4a92ce6c339a4124132534135552bd7cc3d3345
MJSibuea <manihar.sibuea@gmail.com>
2023-04-15 11:32:39 +0700

    Untuk Niomic
    Latihan Operator Aritmatika

---
+++ latihan_operator_aritmatika.py
@@ -0,0 +1,16 @@
+# soal no. 1.
+s = 40
+m = 24
+e = 19
+n = 15
+x = s - (m + e - n)
+print('Jumlah siswa yang tidak menyukai pelajaran Matematika dan Bahasa Inggris =',x)
+
+# soal no. 2.
+harga1 = 6000
+harga2 = 9000
+print('2x+y =',harga1)
+print('2x+3y =',harga2)
+print('harga 1 buku adalah = Rp',(3*harga1 - harga2) / 4)
+
+
